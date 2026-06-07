68fdaf227fa65a4f5ec32a868a1d439988b1f747
Jacques Morel <jmorel33@hotmail.com>
2026-06-07 17:20:18 -0400

    Delete build directory

--- build/_sizeof_real.c
+++
@@ -1,9 +0,0 @@
-#define SITUATION_USE_VULKAN
-#define SITUATION_ENABLE_SHADER_COMPILER
-#include "sit/situation_impl_decl.h"
-#include <stdio.h>
-int main(void) {
-    printf("sizeof(_SituationVkAsyncShaderLoad)=%zu\n", sizeof(_SituationVkAsyncShaderLoad));
-    printf("sizeof(_SituationSpirvBlob)=%zu\n", sizeof(_SituationSpirvBlob));
-    return 0;
-}
