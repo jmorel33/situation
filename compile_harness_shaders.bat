120bcb7be9cd1162110d63b1192b4c2fd92a53e5
Jacques Morel <jmorel33@hotmail.com>
2026-06-30 21:28:14 -0400

    Delete compile_harness_shaders.bat

--- compile_harness_shaders.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Precompile harness test shaders to SPIR-V (OpenGL + Vulkan targets).
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0compile_harness_shaders.ps1"
-exit /b %ERRORLEVEL%
